0cdc8c34f9424d13139c4d960e2d5c7b535bf7b1
smahot <steve.mahot@edu.devinci.fr>
2019-02-26 18:57:26 +0100

    mise en page

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
+# Application de l'algorithme Apriori 
+# Fait par Matthieu LOUF et Steve MAHOT 
 
-def afficher_tab(tab):
-    for i in range(len(tab)):
-        if tab[i]!=[[],[]]:
-            for j in range(len(tab[i][0])):
-                print(tab[i][0][j])
+# Définition des variables :
+
+## Nombre d'occurences minimal
+epsilon = 3
+
+## Datasets (nombres ou mots): 
+dataset_1 = [
+    [1,2,5],
+    [1,3,5],
+    [1,2],
+    [1,2,3,4,5],
+    [1,2,4,5],
+    [2,3,5],
+    [1,5]
+]
+
+dataset_2 = [
+    ["arbre", "orange", "thé", "banane"],
+    ["carotte", "fruit", "orange", "banane", "thé"],
+    ["carotte", "thé", "fruit"],
+    ["arbre", "thé", "patate"],
+    ["crevette", "fruit", "banane"],
+    ["arbre", "banane", "thé", "orange", "crevette"],
+    ["patate", "crevette", "thé", "banane", "arbre"],
+    ["fruit", "thé", "banane", "crevette"]
+]
+
+# Définition des fonctions
+
+# affiche les résultats contenu dans la liste "tab"
+def afficher_resultat(list_Lk):
+    for i in range(len(list_Lk)):
+        if list_Lk[i] != [[],[]]:
+            for j in range(len(list_Lk[i][0])):
+                print(list_Lk[i][0][j])
             
 
 def recherche_ind(tableau, nb):
     indice = 0
     for i in range(len(tableau)):
-        if(tableau[i]==nb):
-            indice =i
+        if(tableau[i] == nb):
+            indice = i
     return indice
 
+# supprime les items identique dans une liste donnée
 def remove_duplicate(d):
     return [i for n, i in enumerate(d) if i not in d[n + 1:]]
 
+# renvoie Ck+1 à partir de Lk
 def sous_ensembles (liste, k):
     res = list()
     if k == 2:
@@ -34,12 +68,13 @@ def sous_ensembles (liste, k):
                 if len(res[len(res)-1]) != k:
                     del res[len(res)-1]
 
-                res[len(res)-1].sort()
+                if len(res) > 0:
+                    res[len(res)-1].sort()
         res.sort()
         res = remove_duplicate(res)
     return res
 
-#renvoie Cn avec le sup : fait le scan D
+# renvoie Ck avec le support
 def calcule_occurences (set, liste):
     res = list()
     res.append(liste)
@@ -50,116 +85,60 @@ def calcule_occurences (set, liste):
             tot = 0
             for k in range (len(liste[i])):
                 if liste[i][k] in set[j]:
-                    tot+=1
+                    tot += 1
             if tot == len(liste[i]):
                 res[1][i] += 1
     return res
 
+# supprime les ensembles dont l'occurence est inférieur à celui donné en paramètre
 def elimination_ensembles(tab, min_occurences):
     tab_final = [[],[]]
 
     for i in range(len(tab[0])):
-        if(tab[1][i])>=min_occurences:
+        if(tab[1][i]) >= min_occurences:
             tab_final[0].append(tab[0][i])
             tab_final[1].append(tab[1][i])
     
     return tab_final
 
-def apriori(transactions, min_occurences):  
-    
-    #Calcul de C1
-    C1 =[[],[]]
-    for i in range(len(transactions)):
-        for j in range(len(transactions[i])):
-            #print(transactions[i][j])
-            if C1[0].count(transactions[i][j]) == 0:
-                C1[0].append(transactions[i][j])
+# fonction principale de l'algorithme Apriori
+def apriori(dataset, min_occurences): 
+    # Calcul de C1
+    C1 = [[],[]]
+    for i in range(len(dataset)):
+        for j in range(len(dataset[i])):
+            if C1[0].count(dataset[i][j]) == 0:
+                C1[0].append(dataset[i][j])
                 C1[1].append(0)
-            C1[1][recherche_ind(C1[0],transactions[i][j])] +=1
+            C1[1][recherche_ind(C1[0],dataset[i][j])] += 1
     
-    #print("C1 : ")
-    #print(C1)
-    
-    
-    #Calcul de L1
-    L_tab = [elimination_ensembles(C1,min_occurences)]
-    #print("L1 : ")
-    #print(L_tab)
+    # Calcul de L1
+    Lk = [elimination_ensembles(C1,min_occurences)]
 
-    k=2
-    C_tab_k=C1
+    k = 2
+    Ck = C1
 
-    #On peut commencer la boucle while principale
-    
-    while L_tab[k-2]!=[[],[]]:
+    # On peut commencer la boucle while principale
+    # Elle s'arrête lorsque la fonction elimination_ensemble() ne renvoie rien
+    while Lk[k-2] != [[],[]]:
         # Formation de tous les ensembles de taille k possibles à partir du Lk précédent
-        ensembles_possibles_temp = sous_ensembles(L_tab[k-2][0], k)
+        ensembles_possibles_temp = sous_ensembles(Lk[k-2][0], k)
 
         # On rajoute un count à tous les ensembles possibles trouvés
-        C_tab_k = calcule_occurences(transactions, ensembles_possibles_temp)
+        Ck = calcule_occurences(dataset, ensembles_possibles_temp)
 
         # On élimine les ensembles dont le nombre d'occurences est inférieur au paramètre
-        L_k = elimination_ensembles(C_tab_k,min_occurences)
-        
-        """print("\n Tour : ",k-2)
-        print(ensembles_possibles_temp)
-        print(C_tab_k)
-        print(L_k)"""
+        L_k = elimination_ensembles(Ck,min_occurences)
 
         # On rajoute le groupe d'ensembles trouvés au tableau d'union des Lk
-        L_tab.append(L_k)
-        k=k+1
-        
+        Lk.append(L_k)
+        k = k+1
 
-    return L_tab
-
-
-# Utilisation de l'algorithme pour l'ensemble de transactions T donné
-transactions_set = [
-    [1,2,5],
-    [1,3,5],
-    [1,2],
-    [1,2,3,4,5],
-    [1,2,4,5],
-    [2,3,5],
-    [1,5]
-]
-
-epsilon = 3
-print("\n Les sous-ensembles d'items frequents avec un nombre d'occurrences > ",epsilon," :\n")
-resultat=apriori(transactions_set,epsilon)
-afficher_tab(resultat)
+    return Lk
 
 
-"""
-#Tests unitaires & cie:
+# Execution de l'algorithme et affichage des résultats
 
-print("Test")
-L1 = [1,2,3]
-L2 = [[1,2], [1,3], [1,5], [2,3], [2,5], [3,5]]
-
-print(len(L1))
-print(len(L2))
-print(len(L2[0]))
-
-print("\nsous_ensembles :")
-res1 = sous_ensembles(L1, 2)
-res2 = sous_ensembles(L2, 3)
-print(res1)
-print(res2)
-
-print("\noccur :")
-print(calcule_occurences(transactions_set, res1))
-print(calcule_occurences(transactions_set, res2))
-
-print("\n Elimination possibilités <3")
-test = [[[1, 2], [1, 5], [1, 3], [2, 5], [2, 3], [5, 3]], [4, 5, 2, 4, 2, 3]]
-print(test)
-print(elimination_possibilites(test,3))
-
-print("\nL1, L2 :")
-print(L1)
-print(L2)
-"""
-
-print("\n")
+print("\n Les sous-ensembles d'items fréquents avec un nombre d'occurrences > ", epsilon," :\n")
+resultat=apriori(dataset_1,epsilon)
+afficher_resultat(resultat)
